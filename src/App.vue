29a72bb19fe8062a36875ca9a575b689e2ab5e72
HJ.Y <hdyanghj@gmail.com>
2021-03-11 16:12:57 +0800

    橘子
    添加项目

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <Index msg="个人简历" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Index from './components/index.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Index
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
---
+++ src/components/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <Info/>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+import Info from './info.vue'
+export default defineComponent({
+  name: 'HelloWorld',
+  components:{
+	  Info
+  },
+  props: {
+    msg: {
+      type: String,
+      required: true
+    }
+  },
+  setup: () => {
+    const count = ref(0)
+    return { count }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
