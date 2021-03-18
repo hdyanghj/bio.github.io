7bbecb4991e69f0e172e9c504c8feca47103ed77
HJ.Y <hdyanghj@gmail.com>
2021-03-18 15:55:13 +0800

    橘子

---
+++ Jack-Pot/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <JackpotSoccer msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import JackpotSoccer from './components/jackpot-soccer.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    JackpotSoccer
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
